156d1f0cc82f3b500ff4efc827ef888558f6f095
Michał Czopek <44530932+Grabsky@users.noreply.github.com>
2025-06-14 21:51:08 +0200

    add missing files (git excluded them for some reason)

---
+++ src/main/java/cloud/grabsky/displayentities/command/CommandDisplayRotateY.java
@@ -0,0 +1,60 @@
+/*
+ * DisplayEntities (https://github.com/Grabsky/DisplayEntities)
+ *
+ * MIT License
+ *
+ * Copyright (c) 2025 Grabsky (michal.czopek.foss@proton.me)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package cloud.grabsky.displayentities.command;
+
+import cloud.grabsky.displayentities.DisplayWrapper;
+import cloud.grabsky.displayentities.configuration.PluginConfiguration;
+import cloud.grabsky.displayentities.util.LombokExtensions;
+import org.bukkit.entity.Player;
+import revxrsal.commands.annotation.Command;
+import revxrsal.commands.annotation.Dependency;
+import revxrsal.commands.bukkit.annotation.CommandPermission;
+
+import org.jetbrains.annotations.NotNull;
+
+import lombok.experimental.ExtensionMethod;
+
+@ExtensionMethod(LombokExtensions.class)
+public enum CommandDisplayRotateY {
+    INSTANCE; // SINGLETON
+
+    @Dependency
+    private PluginConfiguration configuration;
+
+    @Command("display edit <display> rotate_y")
+    @CommandPermission("displayentities.command.display.edit.rotate_y")
+    public String onDisplayRotateY(
+            final @NotNull Player sender,
+            final @NotNull DisplayWrapper display,
+            final @NotNull Float degrees
+    ) {
+        // Teleporting entity to desired location.
+        display.entity().setRotation(display.entity().getYaw() + degrees, display.entity().getPitch());
+        // Returning (sending) success message to the sender.
+        return configuration.messages().commandDisplayEditRotateYSuccess().repl("{yaw}", String.format("%.2f", display.entity().getYaw()));
+    }
+
+}
